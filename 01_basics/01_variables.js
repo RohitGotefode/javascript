daedc82fdb81c9d1555fdba37980ad7a7c42a0d2
RohitGotefode <113708341+RohitGotefode@users.noreply.github.com>
2023-11-01 13:55:08 +0000

    var and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 42762
+let accountEmail = "rohit@gmail.com"
+var accountPassword = "27426"
+accountCity = "delhi"
+
+// accountId = 2 // not allowed
+accountEmail = "dqvdv@gmail.com"
+accountPassword = "4684"
+accountCity = "Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* 
+Not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+// alert(3+3) // if i use nodejs then code is not exicute. if i run on browser then code is run
+
+let name = "Rohit"  // string
+let age = 18  // integer
+let isLoggedIn = false // boolean
+let state ; // undefined
+//null stand alone value
+// symbol = unique
+//object
+
+console.log(typeof "rohit"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
