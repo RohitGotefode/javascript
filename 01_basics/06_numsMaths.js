e074de208a16b7b32903e52e2feab7430e76dc5e
RohitGotefode <113708341+RohitGotefode@users.noreply.github.com>
2023-11-06 06:28:16 +0000

    nums and maths

---
+++ 01_basics/06_numsMaths.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new number(100)
+
+
+// console.log(balance); //[number : 100]
+
+// console.log(balance.tostring().length); // 3
+// console.log(balance.tofixed(1));
+
+// const otherNumber = 123.365
+// console.log(otherNumber.toPrecision(3)); // 124
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN')); // 1,00,000
+
+
+// **********Maths************
+console.log(Maths);
+console.log(Maths.abs(-4)); //4
+console.log(Maths.round(4.6)); // 5
+console.log(Maths.ceil(4.2)); // 5
+console.log(Maths.floor(4.9)); 4
+console.log(Maths.min(4, 3, 2, 6));
+console.log(Maths.max(4, 3, 2, 6));
+
